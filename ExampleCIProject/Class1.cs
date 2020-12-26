b1826898ddce709dd2af9335eb44608b4a03cf34
BobH <bobh@zendee.cn>
2020-12-26 22:47:40 +0800

    meg

--- ExampleCIProject/Class1.cs
+++ ExampleCIProject/Class1.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using UnityEngine;
 namespace ExampleCIProject
 {
     public class Class1
     {
+        GameObject aa;
         //test
     }
 }
